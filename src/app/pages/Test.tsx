760f99b06f1441de82ebbae3d88861e38983f171
atomicjed <jed.ryan@atomicmedia.co.uk>
2024-11-24 16:55:44 +0000

    Fix build

--- src/app/pages/Test.tsx
+++ src/app/pages/Test.tsx
@@ -1,7 +1,4 @@
-import {useSpeech} from "../lib/context/accessibility.context.tsx";
-
 export default function TestPage() {
-  const {updatePageScript} = useSpeech();
   return (
     <div>Test Page</div>
   )
